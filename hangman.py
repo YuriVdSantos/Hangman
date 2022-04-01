ff00aa736d5a78d07803adde62f1d7948c93a93f
Santos <yurivdsantos@outlook.com>
2022-04-01 13:18:44 -0300

    base do jogo da forca

---
+++ hangman.py
@@ -0,0 +1,15 @@
+import random
+
+print("Palavra com 4 letras")
+print("todo mundo já falou ou ouviu falar")
+print("É um dos maiores temas de filmes e séries")
+i = input("Digite uma letra ou a palavra que acha: ")
+
+if i == "amor" or  i == "Amor":
+    print("Você acertou!")
+
+elif i == "a":
+    print("a***")
+else:
+    print("Sua cabeça já está na corda")
+    
