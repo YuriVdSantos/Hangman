548338b0d5f2e71de21d1f8492235bd62b93f60d
Santos <yurivdsantos@outlook.com>
2022-04-02 23:51:04 -0300

    Loop para variação de resposta inserido

--- hangman.py
+++ hangman.py
@@ -4,12 +4,27 @@ print("Palavra com 4 letras")
 print("todo mundo já falou ou ouviu falar")
 print("É um dos maiores temas de filmes e séries")
 i = input("Digite uma letra ou a palavra que acha: ")
+c = 0
 
-if i == "amor" or  i == "Amor":
-    print("Você acertou!")
+while c < 5:
+    if i == "amor" or  i == "Amor":
+        print("Você acertou!")
 
-elif i == "a":
-    print("a***")
-else:
-    print("Sua cabeça já está na corda")
-    
+    elif i == "a":
+        print("a***")
+        c = c + 1
+    elif i == "m":
+        print("*m***")
+        c = c + 1
+    elif i == "o":
+        print("**o*")
+        c = c + 1
+    elif i == "r":
+        print("***r")
+        c = c + 1
+    else:
+        print("Sua cabeça já está na corda")
+        i = input("Digite uma letra ou a palavra que acha: ")
+        c = c + 1
+
+        
