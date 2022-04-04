7fd76e062354837c0c5d686c0ddca7a21bd6924d
Santos <yurivdsantos@outlook.com>
2022-04-03 23:27:10 -0300

    Estrutura base do jogo pronta. Com adição de instruções por voz

--- hangman.py
+++ hangman.py
@@ -1,5 +1,35 @@
-import random
+import pyttsx3
 
+fala = pyttsx3.init()
+
+fala.say("Olá humano, bem-vido ao jogo da Forca! Por favor, digite a palavra que você deseja adivinhar"
+", mas sem que seus amiginhos vejam")
+fala.runAndWait()
+
+palavra = input("Digite a palavra: ")
+
+fala.say("Agora espere seus amiginhos adivinhar a palavra, ou errar kkkkkk")
+fala.runAndWait()
+
+print("Fica a dica a palavra tem {} letras".format(len(palavra)))
+
+erros = 0
+
+while erros <5:
+    letra = input("Digite uma letra: ")
+    if letra in palavra:
+        print("A letra {} está na palavra".format(letra))
+    else:
+        print("A letra {} não está na palavra".format(letra))
+        erros += 1
+        print("Você tem {} erros".format(erros))
+        if erros == 5:
+            print("Você perdeu")
+            fala.say("Você perdeu")
+            fala.runAndWait()
+            break
+
+'''
 print("Palavra com 4 letras")
 print("todo mundo já falou ou ouviu falar")
 print("É um dos maiores temas de filmes e séries")
@@ -26,5 +56,4 @@ while c < 5:
         print("Sua cabeça já está na corda")
         i = input("Digite uma letra ou a palavra que acha: ")
         c = c + 1
-
-        
+'''
